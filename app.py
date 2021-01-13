e1d59dd6f7b103e3007c497ddceb82d01034fd68
Faisal Khan <fai786salkhan@gmail.com>
2021-01-13 20:37:45 +0530

    init

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flaskext.mysql import MySQL
 import pymysql
 import io
 import csv
+import pandas as pd
+import pyodbc 
  
 app = Flask(__name__)
 app.secret_key = "Faisal"
@@ -73,36 +75,6 @@ def update_employee(id):
         flash('Employee Updated Successfully')
         conn.commit()
         return redirect(url_for('Index'))
-
-# @app.route('/search_user', methods=['POST', 'GET'])
-# def search_user():
-#     conn = mysql.connect()
-#     cur = conn.cursor(pymysql.cursors.DictCursor)
-#     if request.method == 'POST':
-#         id = request.form['id']
-#         return redirect('search.html/%s',id)
-
-# @app.route('/search/<id>', methods = ['GET',])
-# def show_user(id):
-#     conn = mysql.connect()
-#     cur = conn.cursor(pymysql.cursors.DictCursor)
-  
-#     cur.execute('SELECT * FROM employee WHERE id = %s', (id))
-#     data = cur.fetchall()
-#     cur.close()
-#     print(data[0])
-#     return render_template('search.html', employee = data[0])
-
-# @app.route('/search/<id>', methods = ['POST', 'GET'])
-# def get_userbyid(id):
-#     conn = mysql.connect()
-#     cur = conn.cursor(pymysql.cursors.DictCursor)
-  
-#     cur.execute('SELECT * FROM employee WHERE id = %s', (id))
-#     data = cur.fetchall()
-#     cur.close()
-#     print(data[0])
-#     return render_template('search.html', employee = data[0])
  
 @app.route('/delete/<string:id>', methods = ['POST','GET'])
 def delete_employee(id):
@@ -114,35 +86,41 @@ def delete_employee(id):
     flash('Employee Removed Successfully')
     return redirect(url_for('Index'))
 
-@app.route('/download/report/csv')
+@app.route('/download')
 def download_report():
- conn = None
- cursor = None
- try:
-  conn = mysql.connect()
-  cursor = conn.cursor(pymysql.cursors.DictCursor)
+    conn = mysql.connect()
+    df = pd.read_sql('SELECT * FROM employee', conn)
+    df.to_csv (r'C:/export_data.csv', index = False) 
+    conn.commit()
+    return redirect(url_for('Index'))
+
+#  conn = None
+#  cursor = None
+#  try:
+#   conn = mysql.connect()
+#   cursor = conn.cursor(pymysql.cursors.DictCursor)
    
-  cursor.execute("SELECT id, first_name, last_name, designation FROM employees")
-  result = cursor.fetchall()
+#   cursor.execute("SELECT id, first_name, last_name, designation FROM employees")
+#   result = cursor.fetchall()
  
-  output = io.StringIO()
-  writer = csv.writer(output)
+#   output = io.StringIO()
+#   writer = csv.writer(output)
    
-  line = ['Id, First Name, Last Name, Designation']
-  writer.writerow(line)
+#   line = ['Id, First Name, Last Name, Designation']
+#   writer.writerow(line)
  
-  for row in result:
-   line = [str(row['id']) + ',' + row['first_name'] + ',' + row['last_name'] + ',' + row['designation']]
-   writer.writerow(line)
+#   for row in result:
+#    line = [str(row['id']) + ',' + row['first_name'] + ',' + row['last_name'] + ',' + row['designation']]
+#    writer.writerow(line)
  
-  output.seek(0)
+#   output.seek(0)
    
-  return response(output, mimetype="text/csv", headers={"Content-Disposition":"attachment;filename=employee_report.csv"})
- except Exception as e:
-  print(e)
- finally:
-  cursor.close() 
-  conn.close()
+#   return response(output, mimetype="text/csv", headers={"Content-Disposition":"attachment;filename=employee_report.csv"})
+#  except Exception as e:
+#   print(e)
+#  finally:
+#   cursor.close() 
+#   conn.close()
  
 # starting the app
 if __name__ == "__main__":
